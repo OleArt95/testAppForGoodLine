081a9e5b01897eee070e6e434989c6b02230cd54
Oleg Art <Wickodin@gmail.com>
2019-03-06 23:21:38 +0700

    Написал hello world при помощи Selenium

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Артюхов Олег\\IdeaProjects\\testAppForGoodLine\\selen\\chromedriver.exe");
+        WebDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("http://цг.рф");
 
     }
 }
