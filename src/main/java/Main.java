c20f009244322b2059df5e260786991a299c5bc1
Oleg Art <Wickodin@gmail.com>
2019-03-07 23:17:53 +0700

    Изменения: 1. Вынес WebDriver в статическую переменную 2. Добавил метод clickOnIconCityDelivery для нажатия на кнопку городская доставка 3. Добавил метод clickOnLinkKnowMoreCityDelivery для нажатия на кнопку "Узнать больше" в блоке "Городская доставка" 4. Добавил вызов реализованных методов внутри метода main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
+    private static WebDriver chromeDriver;
     private static final String WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
     private static final String URL_FOR_DIGITAL_CITY = "http://цг.рф";
     private static final String PATH_TO_CHROME_DRIVER = ".\\selen\\chromedriver.exe";
@@ -9,8 +12,19 @@ public class Main {
     public static void main(String[] args) {
 
         System.setProperty(WEBDRIVER_CHROME_DRIVER, PATH_TO_CHROME_DRIVER);
-        WebDriver chromeDriver = new ChromeDriver();
+        chromeDriver = new ChromeDriver();
         chromeDriver.get(URL_FOR_DIGITAL_CITY);
+        clickOnIconCityDelivery();
+        clickOnLinkKnowMoreCityDelivery();
 
+    }
+    private static void clickOnIconCityDelivery() {
+        WebElement iconCityDelivery = chromeDriver.findElement(By.cssSelector("[data-id=\"3\"] button.switcher__button"));
+        iconCityDelivery.click();
+    }
+
+    private static void clickOnLinkKnowMoreCityDelivery() {
+        WebElement linkContentMore = chromeDriver.findElement(By.cssSelector("[href=\"#food\"]"));
+                linkContentMore.click();
     }
 }
