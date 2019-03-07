5a070b2ecf586700e2bca58dc1c670d0573c2bfd
Oleg Art <Wickodin@gmail.com>
2019-03-07 21:47:56 +0700

    Создал константы: WEBDRIVER_CHROME_DRIVER, URL_FOR_DIGITAL_CITY, PATH_TO_CHROME_DRIVER

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
+    private static final String WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
+    private static final String URL_FOR_DIGITAL_CITY = "http://цг.рф";
+    private static final String PATH_TO_CHROME_DRIVER = ".\\selen\\chromedriver.exe";
+
     public static void main(String[] args) {
-//        System.out.println("Hello world!");
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Артюхов Олег\\IdeaProjects\\testAppForGoodLine\\selen\\chromedriver.exe");
+
+        System.setProperty(WEBDRIVER_CHROME_DRIVER, PATH_TO_CHROME_DRIVER);
         WebDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get("http://цг.рф");
+        chromeDriver.get(URL_FOR_DIGITAL_CITY);
 
     }
 }
