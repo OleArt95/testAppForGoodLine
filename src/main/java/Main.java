631d46608d85052bae846aca19190a86fabae428
Oleg Art <Wickodin@gmail.com>
2019-03-09 01:36:40 +0700

    Изменения:
    1. Объявил переменную для драйвера ожидания
    2. Добавил метод switchToTabCityDelivery для переключения на новую
    вкладку сайта "Городская доставка" в основном окне браузера
    3. Добавил константу TAB_CITY_DELIVERY
    4. Добавил метод sleepGeneralThread ожидания основного потока
    5. Добавил метод clickOnCloseButtonOnModalWindowCityDelivery для нажатия
    на кнопку закрытия в модальном окне на сайте "Городская доставка"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
 
 public class Main {
     //Объявление переменной для веб-драйвера
     private static WebDriver chromeDriver;
+
+    private static WebDriverWait waitDriver;
+
+    private static final int TAB_CITY_DELIVERY = 1;
 
     //Объявление константы для драйвера
     private static final String WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
@@ -22,9 +30,12 @@ public class Main {
         chromeDriver = new ChromeDriver();
         chromeDriver.get(URL_FOR_DIGITAL_CITY);
 
+        waitDriver = new WebDriverWait(chromeDriver, 60);
+
         clickOnIconCityDelivery();
         clickOnLinkKnowMoreCityDelivery();
         clickOnLinkDigitalCityInfoBlockCityDelivery();
+        clickOnCloseButtonOnModalWindowCityDelivery();
     }
 
     /**
@@ -47,12 +58,37 @@ public class Main {
      * Метод для нажатия на кнопку "Узнать больше" на сайте "Цифровой город" в разделе "Городская доставка".
      */
     private static void clickOnLinkDigitalCityInfoBlockCityDelivery() {
+        long timeToWait = 1000L;
+
         WebElement linkContentMoreAboutExternalSite = chromeDriver.findElement(By.cssSelector("[href=\"https://dostavka.a42.ru/?utm_source=digitalcity\"]"));
+
+        sleepGeneralThread(timeToWait);
+
+        linkContentMoreAboutExternalSite.click();
+    }
+
+    private static void clickOnCloseButtonOnModalWindowCityDelivery() {
+        long timeToWait = 5000L;
+
+        switchToTabCityDelivery();
+
+        WebElement closeButtonOnModalWindow = waitDriver.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[class=\"sendpulse-prompt-close\"]")));
+
+        sleepGeneralThread(timeToWait);
+
+        closeButtonOnModalWindow.click();
+    }
+
+    private static void switchToTabCityDelivery(){
+        ArrayList<String> tabs = new ArrayList<String>(chromeDriver.getWindowHandles());
+        chromeDriver.switchTo().window(tabs.get(TAB_CITY_DELIVERY));
+    }
+
+    private static void sleepGeneralThread(long millisecondsOfWait){
         try {
-            Thread.sleep(5000L);
+            Thread.sleep(millisecondsOfWait);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        linkContentMoreAboutExternalSite.click();
     }
 }
